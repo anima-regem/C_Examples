6d892f045b405b634c071863ccdecfd7490636a7
anima-regem <vichukartha@gmail.com>
2022-07-11 01:44:59 +0530

    Added 3 Programs

---
+++ 3_prime.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,flag=0;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    for(i=2;i<(n/2);i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+        printf("The number is not prime.\n");
+    else
+        printf("The number is prime.\n");
+}
